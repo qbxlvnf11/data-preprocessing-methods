55194aa0c742eabf782ca5461c6df6b861fb10a5
Yong <qbxlvnf11@naver.com>
2025-06-22 07:36:14 +0900

    Update video_cutting_seconds.py

--- Image Processing/video_cutting_seconds.py
+++ Image Processing/video_cutting_seconds.py
@@ -1,4 +1,5 @@
-from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+# from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+from moviepy import VideoFileClip
 import cv2
 import argparse
 import os
@@ -16,15 +17,41 @@ def parse_args():
 
 args = parse_args()
   
-# path
-video_path = args.video_path
-save_path = args.save_path
+# # path
+# video_path = args.video_path
+# save_path = args.save_path
 
-# Cut seconds
-start_seconds = args.start_seconds
-end_seconds = args.end_seconds
+# # Cut seconds
+# start_seconds = args.start_seconds
+# end_seconds = args.end_seconds
 
-video = cv2.VideoCapture(video_path)
+# # video = cv2.VideoCapture(video_path)
 
-# ffmpeg_extract_subclip(video_path, start_seconds, end_seconds, save_path)
-ffmpeg_extract_subclip(video_path, start_seconds, end_seconds, targetname=save_path)
+# # ffmpeg_extract_subclip(video_path, start_seconds, end_seconds, save_path)
+# ffmpeg_extract_subclip(video_path, start_seconds, end_seconds, targetname=save_path)
+
+def cut_video(video_path, save_path, start_seconds, end_seconds):
+
+    print(f"  -> MoviePy로 영상 자르는 중: {os.path.basename(save_path)}")
+
+    try:
+        with VideoFileClip(video_path) as video:
+            
+            highlight_clip = video.subclipped(start_seconds, end_seconds)
+            
+            highlight_clip.write_videofile(
+                save_path, 
+                codec="libx264",        
+                audio_codec="aac"       
+            )
+        
+        print(f"  -> 저장 완료: {save_path}")
+
+    except Exception as e:
+        print(f"  -> MoviePy 작업 중 오류 발생: {e}")
+
+cut_video(
+    video_path=args.video_path, \
+    save_path=args.save_path, \
+    start_seconds=args.start_seconds, \
+    end_seconds=args.end_seconds)
